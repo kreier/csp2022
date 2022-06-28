35bd88edf25438ddfd93a0d0eefcf7bd59de2c8e
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-06-29 00:28:39 +0700

    fixed, works

--- groupsmembers/assignment.py
+++ groupsmembers/assignment.py
@@ -1,6 +1,25 @@
-# test example github.com/kreier/csp2022
+# Test example github.com/kreier/csp2022
+
+import random
 
 print('How many students are in the class: ', end='')
-nr_students = input()
+nr_students = int(input())
 
-print(f'The number of students is {nr_students} today.')
+print('How many people per group: ', end='')
+groupsize = int(input())
+
+list = []
+for i in range(nr_students):
+  list.append(i+1)
+
+random.shuffle(list)
+
+# groups able to do
+for i in range(int(nr_students/2)):
+  print(f'{list[2*i]} <--> {list[2*i+1]}')
+
+# remaining students
+remaining = nr_students % groupsize
+
+for i in range(nr_students - remaining, nr_students):
+  print(list[i])
