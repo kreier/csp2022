bb17bf46b826ebcdce03a2c7aa7a924ec38966f6
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-06-29 19:01:56 -0400

    replit edition 2022

---
+++ RandomStudentGenerator/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Reg Hahne
+ * Generating pairs of random numbers
+ * 7/29/2018
+ * 6/10/2019
+ * 1/6/2020
+ */
+
+import java.util.Scanner;
+
+public class Main // was GenerateRandomRunner, but does not work in replit
+{
+	public static void main(String[] args) 
+	{
+		NumGenerator myNumGenerator;
+		int numParticipants, teamSize;
+		Scanner inData = new Scanner(System.in);
+		
+		System.out.print("Please enter the total number of participants --> ");
+		numParticipants = inData.nextInt();
+		System.out.print("Please enter the number of participants per group --> ");
+		teamSize = inData.nextInt();
+		myNumGenerator = new NumGenerator(numParticipants, teamSize);
+		System.out.print(myNumGenerator);
+		inData.close();
+	}
+}
